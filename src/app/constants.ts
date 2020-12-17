715b2f6a15f97839142235cbec725ac0ab3a6853
Chris Barber <chris.barber@moat.co.uk>
2020-12-17 09:29:14 +0000

    Commit source code

---
+++ src/app/constants.ts
@@ -0,0 +1,10 @@
+
+export class Constants {
+	public static clientRoot = 'http://localhost:4200/';
+
+	public static apiRoot = 'TEST_API_URL';
+	
+	public static stsAuthority = 'RISKHUB_DOMAIN';
+
+	public static clientId = 'RISKHUB_CLIENT_ID';
+}
